0a50a6c06063f90ab2a9565b3dde8141547ea2fa
Mathias Schubanz <schubmat@tu-cottbus.de>
2017-02-01 16:05:20 +0100

    Refactoring

--- de/btu/sst/evs/blatt10/drawingLib/shapes/Point.java
+++ de/btu/sst/evs/blatt10/drawingLib/shapes/Point.java
@@ -2,6 +2,14 @@ package de.btu.sst.evs.blatt10.drawingLib.shapes;
 
 public class Point {
 
+    public Point(int x, int y) {
+	this((double) x, y);
+    }
+
+    public Point(double x, double y) {
+	// TODO Auto-generated constructor stub
+    }
+    
     @Override
     public String toString() {
 	return "";
